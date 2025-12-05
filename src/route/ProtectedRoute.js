71935c7dba003fcb36b41acd1e292cb9111eacc4
FullMetal19 <manemamadouyaya@gmail.com>
2025-12-05 18:32:13 +0100

    another commit

--- src/route/ProtectedRoute.js
+++ src/route/ProtectedRoute.js
@@ -10,8 +10,14 @@ export default function ProtectedRoute()
       <div className="d-flex align-items-center justify-content-center vh-100"> 
         <div className="spinner-border text-success fs-1" role="status" aria-label="Chargement"></div> 
       </div>
-     )
-  }; 
+     );
+  }
 
-  return user ? <Outlet /> : <Navigate to="/404" replace />;
+  // If no user, redirect to external URL
+  if (!user) {
+    window.location.href = "https://agrospace.xelkoomai.sn";
+    return null; // Render nothing while redirecting
+  }
+
+  return <Outlet />;
 }
